8a5d2439c35197b5f3f65cedc7e5b7681cae0efe
LizRuelas <liz.ruelas.borda@gmail.com>
2016-09-16 09:56:32 -0700

    modificando cri

--- js/app.js
+++ js/app.js
@@ -1,18 +1,6 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("cri");
 	boton.addEventListener("click", function() {
-		document.getElementById("color1").style.border = "3px solid red";
-
-		document.getElementById("color2").style.border = "0px solid red";
-		document.getElementById("color3").style.border = "0px solid red";
-		
-		document.getElementById("color1").style.border = "0px solid red";
-		document.getElementById("color2").style.border = "3px solid red";
-		document.getElementById("color3").style.border = "0px solid red";
-	
-		document.getElementById("color1").style.border = "0px solid red";
-		document.getElementById("color2").style.border = "0px solid red";
-		document.getElementById("color3").style.border = "3px solid red";
 		
 	});
 
@@ -21,17 +9,17 @@ window.addEventListener("load", function() {
 		
 	var color = document.getElementById("input").value;
 	if (color=="blue"){
-		document.getElementById("color1").style.border = "3px solid yellow";
-		document.getElementById("color2").style.border = "0px solid yellow";
-		document.getElementById("color3").style.border = "0px solid yellow";
+		document.getElementById("color1").style.border = "5px solid red";
+		document.getElementById("color2").style.border = "0px solid red";
+		document.getElementById("color3").style.border = "0px solid red";
 	} else if (color=="green"){
-		document.getElementById("color1").style.border = "0px solid yellow";
-		document.getElementById("color2").style.border = "3px solid yellow";
-		document.getElementById("color3").style.border = "0px solid yellow";
+		document.getElementById("color1").style.border = "0px solid red";
+		document.getElementById("color2").style.border = "5px solid red";
+		document.getElementById("color3").style.border = "0px solid red";
 	} else if (color=="black"){
-		document.getElementById("color1").style.border = "0px solid yellow";
-		document.getElementById("color2").style.border = "0px solid yellow";
-		document.getElementById("color3").style.border = "3px solid yellow";
+		document.getElementById("color1").style.border = "0px solid red";
+		document.getElementById("color2").style.border = "0px solid red";
+		document.getElementById("color3").style.border = "5px solid red";
 	}
 		
 	});
